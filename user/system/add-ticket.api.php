ed9d3f580a8ff2b8a8952476a6eb45065062607d
Nattanon Kh <nattanondev@gmail.com>
2024-03-12 23:18:50 +0700

    connect database

--- user/system/add-ticket.api.php
+++ user/system/add-ticket.api.php
@@ -1,5 +1,22 @@
 <?php
-
+    require_once '../../db-conn.php';
     $request = json_decode(file_get_contents('php://input'));
 
-    echo json_encode($request);
+    $type = $request->type;
+    $topic = $request->topic;
+    $detail = $request->detail;
+
+    $data = array(
+        'tick_type' => $type,
+        'tick_topic' => $topic,
+        'tick_detail' => $detail,
+        'tick_datetime' => date('Y-m-d H:i:s')
+    );
+    $id = $db->insert('ticket', $data);
+    if($id){
+        $api = array('status' => 'success', 'id' => $id);
+    } else {
+        $api = array('status' => 'error');
+    }
+        
+    echo json_encode($api);
