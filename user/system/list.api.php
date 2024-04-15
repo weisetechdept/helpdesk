e02c350c909edcd754d736be90306b1a59052c0e
Nattanon Kh <nattanondev@gmail.com>
2024-04-15 23:05:51 +0700

    test login v0.28

--- user/system/list.api.php
+++ user/system/list.api.php
@@ -1,36 +1,44 @@
 <?php
+    session_start();
     require_once '../../db-conn.php';
+    date_default_timezone_set("Asia/Bangkok");
 
-    $list = $db->get('ticket');
+    if(!isset($_SESSION['hd_login'])) {
+        header('Location: /404');
+    } else {
+        $id = $_SESSION['hd_user_id'];
 
-    foreach($list as $row){
-        if($row['tick_type'] == 1){
-            $type = 'อุปกรณ์ IT / Software';
-        } else {
-            $type = 'เครื่องใช้สำนักงาน / อาคารสถานที่';
+        $list = $db->where('tick_owner',$id)->get('ticket');
+
+        foreach($list as $row){
+            if($row['tick_type'] == 1){
+                $type = 'อุปกรณ์ IT / Software';
+            } else {
+                $type = 'เครื่องใช้สำนักงาน / อาคารสถานที่';
+            }
+
+            if($row['tick_branch'] == 1){
+                $branch = 'สำนักงานใหญ่';
+            } else {
+                $branch = 'สาขา';
+            }
+
+            if($row['tick_division'] == '1'){
+                $division = 'ฝ่ายขาย';
+            } elseif($row['tick_division'] == '2') {
+                $division = 'ฝ่ายการตลาด';
+            }
+
+            $api['data'][] = array(
+                $row['tick_id'],
+                $type,
+                $row['tick_topic'],
+                $branch,
+                $division,
+                $row['tick_status'],
+                $row['tick_datetime']
+            );
         }
-
-        if($row['tick_branch'] == 1){
-            $branch = 'สำนักงานใหญ่';
-        } else {
-            $branch = 'สาขา';
-        }
-
-        if($row['tick_division'] == '1'){
-            $division = 'ฝ่ายขาย';
-        } elseif($row['tick_division'] == '2') {
-            $division = 'ฝ่ายการตลาด';
-        }
-
-        $api['data'][] = array(
-            $row['tick_id'],
-            $type,
-            $row['tick_topic'],
-            $branch,
-            $division,
-            $row['tick_status'],
-            $row['tick_datetime']
-        );
     }
 
     echo json_encode($api);
