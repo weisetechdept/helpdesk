c24154428d81c213a61a133027f701a8728d23fc
Nattanon Kh <nattanondev@gmail.com>
2024-04-15 20:56:24 +0700

    test login v0.5

--- auth/system/register.ins.php
+++ auth/system/register.ins.php
@@ -5,5 +5,39 @@
     $fname = $request->f_name;
     $lname = $request->l_name;
     $dept = $request->department;
+    $uid = $request->uid;
 
-    echo $fname . ' ' . $lname . ' ' . $dept;
+    if($fname == '' || $lname == '' || $dept == '0') {
+        $api = array(
+            'status' => 'error',
+            'message' => 'โปรดตรวจสอบ คุณยังไม่ได้กรอกข้อมูลชื่อ นามสกุล หรือ แผนกที่คุณสังกัด'
+        );
+    } elseif($uid == '') {
+        $api = array(
+            'status' => 'error',
+            'message' => 'ไม่สามารถสมัครสมาชิกได้ กรุณาลองใหม่อีกครั้ง'
+        );
+    } else {
+        $data = array(
+            'user_first_name' => $fname,
+            'user_last_name' => $lname,
+            'user_uid' => $uid,
+            'user_permission' => 'officer',
+            'user_datetime' => date('Y-m-d H:i:s')
+        );
+
+        $db->insert('user', $data);
+        if($db->count > 0) {
+            $api = array(
+                'status' => 'success',
+                'message' => 'สมัครสมาชิกสำเร็จ'
+            );
+        } else {
+            $api = array(
+                'status' => 'error',
+                'message' => 'สมัครสมาชิกไม่สำเร็จ'
+            );
+        }
+    }
+
+    echo json_encode($api);
